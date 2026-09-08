7930872c59ab1c7f92e4ab0b4dfa28f6b72bd077
Wa12fa34 <ummabdullah99199@gmail.com>
2026-09-08 19:27:06 +0400

    Add secure owner dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar.jsx";
 import ProtectedTeacherRoute from "./components/ProtectedTeacherRoute.jsx";
+import ProtectedRoleRoute from "./components/ProtectedRoleRoute.jsx";
 
 import Home from "./pages/Home.jsx";
 import Reading from "./pages/Reading.jsx";
@@ -18,6 +19,7 @@ import Login from "./pages/Login.jsx";
 import TeacherLogin from "./pages/TeacherLogin.jsx";
 import Progress from "./pages/Progress.jsx";
 import TeacherDashboard from "./pages/TeacherDashboard.jsx";
+import OwnerDashboard from "./pages/OwnerDashboard.jsx";
 import ClassDetails from "./pages/ClassDetails.jsx";
 import StudentReport from "./pages/StudentReport.jsx";
 import StudentManagement from "./pages/StudentManagement.jsx";
@@ -26,109 +28,26 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/reading" element={<Reading />} />
+        <Route path="/reading/:level" element={<ReadingLevel />} />
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/writing/:level" element={<WritingLevel />} />
+        <Route path="/vocabulary" element={<Vocabulary />} />
+        <Route path="/vocabulary/:level" element={<VocabularyLevel />} />
+        <Route path="/grammar" element={<Grammar />} />
+        <Route path="/grammar/:level" element={<GrammarLevel />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/teacher-login" element={<TeacherLogin />} />
+        <Route path="/progress" element={<Progress />} />
 
-        <Route
-          path="/reading"
-          element={<Reading />}
-        />
-
-        <Route
-          path="/reading/:level"
-          element={<ReadingLevel />}
-        />
-
-        <Route
-          path="/writing"
-          element={<Writing />}
-        />
-
-        <Route
-          path="/writing/:level"
-          element={<WritingLevel />}
-        />
-
-        <Route
-          path="/vocabulary"
-          element={<Vocabulary />}
-        />
-
-        <Route
-          path="/vocabulary/:level"
-          element={<VocabularyLevel />}
-        />
-
-        <Route
-          path="/grammar"
-          element={<Grammar />}
-        />
-
-        <Route
-          path="/grammar/:level"
-          element={<GrammarLevel />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/teacher-login"
-          element={<TeacherLogin />}
-        />
-
-        <Route
-          path="/progress"
-          element={<Progress />}
-        />
-
-        <Route
-          path="/teacher"
-          element={
-            <ProtectedTeacherRoute>
-              <TeacherDashboard />
-            </ProtectedTeacherRoute>
-          }
-        />
-
-        <Route
-          path="/teacher/class/:classId"
-          element={
-            <ProtectedTeacherRoute>
-              <ClassDetails />
-            </ProtectedTeacherRoute>
-          }
-        />
-
-        <Route
-          path="/teacher/student/:studentId"
-          element={
-            <ProtectedTeacherRoute>
-              <StudentReport />
-            </ProtectedTeacherRoute>
-          }
-        />
-
-        <Route
-          path="/teacher/writing/:submissionId"
-          element={
-            <ProtectedTeacherRoute>
-              <WritingAssessment />
-            </ProtectedTeacherRoute>
-          }
-        />
-
-        <Route
-          path="/teacher/students"
-          element={
-            <ProtectedTeacherRoute>
-              <StudentManagement />
-            </ProtectedTeacherRoute>
-          }
-        />
+        <Route path="/owner" element={<ProtectedRoleRoute roles={["owner"]} redirectTo="/teacher-login"><OwnerDashboard /></ProtectedRoleRoute>} />
+        <Route path="/teacher" element={<ProtectedTeacherRoute><TeacherDashboard /></ProtectedTeacherRoute>} />
+        <Route path="/teacher/class/:classId" element={<ProtectedTeacherRoute><ClassDetails /></ProtectedTeacherRoute>} />
+        <Route path="/teacher/student/:studentId" element={<ProtectedTeacherRoute><StudentReport /></ProtectedTeacherRoute>} />
+        <Route path="/teacher/writing/:submissionId" element={<ProtectedTeacherRoute><WritingAssessment /></ProtectedTeacherRoute>} />
+        <Route path="/teacher/students" element={<ProtectedTeacherRoute><StudentManagement /></ProtectedTeacherRoute>} />
       </Routes>
     </BrowserRouter>
   );
